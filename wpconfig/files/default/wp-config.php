83a35062ffd87a71d723ec79af0095d313612838
gruchacz <gruchacz@grutech.com.au>
2017-06-12 22:11:30 +1000

    Create wp-config.php

--- wpconfig/files/default/wp-config.php
+++ wpconfig/files/default/wp-config.php
@@ -14,10 +14,10 @@
  * @package WordPress
  */
 
-define('WP_REDIS_HOST', 'wordpress-2.tfgepv.0001.apse2.cache.amazonaws.com');
+define('WP_REDIS_HOST', 'http://wordpress-2.tfgepv.0001.apse2.cache.amazonaws.com');
  
-define('WP_HOME','wordpress-998390647.ap-southeast-2.elb.amazonaws.com');
-define('WP_SITEURL','wordpress-998390647.ap-southeast-2.elb.amazonaws.com');
+define('WP_HOME','http://wordpress-998390647.ap-southeast-2.elb.amazonaws.com');
+define('WP_SITEURL','http://wordpress-998390647.ap-southeast-2.elb.amazonaws.com');
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
